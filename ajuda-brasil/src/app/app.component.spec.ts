cd6b0c7438aa170d5cd464c23f9b7f58c32b31dd
Wesley Marques <wesleymqsss@gmail.com>
2025-03-15 15:12:52 -0300

    Adicionando bibliotecas ao projeto

--- ajuda-brasil/src/app/app.component.spec.ts
+++ ajuda-brasil/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ajuda-brasil' title`, () => {
+  it(`should have as title 'ajuda-brasil'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ajuda-brasil');
